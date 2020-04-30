4a3caeb41ad22f4da1bfce61865c97e65da7040a
TataR21 <m.tatarski96@gmail.com>
2020-04-30 16:12:16 +0200

    Add new charts

--- src/main/java/com/example/pogoda/TableController.java
+++ src/main/java/com/example/pogoda/TableController.java
@@ -60,7 +60,7 @@ public class TableController {
             String idStation = json.getAsJsonArray("array").get(i).getAsJsonObject().get("id_stacji").toString();
             idStation = idStation.replaceAll("\"","");
             //String z = ;
-           // idStation = idStation.replaceAll("\\\\","");
+            idStation = idStation.replaceAll("\\\\","");
 
             String stationName = json.getAsJsonArray("array").get(i).getAsJsonObject().get("stacja").toString();
             stationName = stationName.replaceAll("\"","");
